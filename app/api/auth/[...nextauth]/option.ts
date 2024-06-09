04a7996d3f56e62063b07c10deb45a8f36efb058
iasadk <89456039+iasadk@users.noreply.github.com>
2024-06-09 16:52:53 +0530

    Another one.

---
+++ app/api/auth/[...nextauth]/option.ts
@@ -0,0 +1,47 @@
+import { Account, User as AuthUser, NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+const authOptions: NextAuthOptions = {
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+        authorization: {
+          params: {
+            prompt: "consent",
+            access_type: "offline",
+            response_type: "code",
+            scope:
+              "https://mail.google.com/ openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
+          },
+        },
+      }),
+      // ...add more providers here
+    ],
+    callbacks: {
+      // @ts-ignore
+      async signIn({ user, account }: { user: AuthUser; account: Account }) {
+        if (account?.provider == "google") {
+          return true;
+        }
+      },
+      async jwt({ token, account }:{token: any, account: any}) {
+        if (account) {
+          token = Object.assign({}, token, {
+            access_token: account.access_token,
+          });
+        }
+        return token;
+      },
+      async session({ session, token }:{session: any, token: any}) {
+        if (session) {
+          session = Object.assign({}, session, {
+            access_token: token.access_token,
+          });
+          console.log(session);
+        }
+        return session;
+      },
+    },
+  };
+
+  export default authOptions;
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,49 +1,8 @@
-import NextAuth, { Account, User as AuthUser, NextAuthOptions, TokenSet } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth";
+import authOptions from "./option";
 
-const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-          scope:
-            "https://mail.google.com/ openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
-        },
-      },
-    }),
-    // ...add more providers here
-  ],
-  callbacks: {
-    // @ts-ignore
-    async signIn({ user, account }: { user: AuthUser; account: Account }) {
-      if (account?.provider == "google") {
-        return true;
-      }
-    },
-    async jwt({ token, account }:{token: any, account: any}) {
-      if (account) {
-        token = Object.assign({}, token, {
-          access_token: account.access_token,
-        });
-      }
-      return token;
-    },
-    async session({ session, token }:{session: any, token: any}) {
-      if (session) {
-        session = Object.assign({}, session, {
-          access_token: token.access_token,
-        });
-        console.log(session);
-      }
-      return session;
-    },
-  },
-};
+
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
+
